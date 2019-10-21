3530d23a37fc9a02da4b66498199ef75f03ec734
Maskonk <pincer1@hotmail.co.uk>
2019-10-21 23:31:43 +0100

    Moved Dnd class to its own file

---
+++ Dnd.py
@@ -0,0 +1,27 @@
+from discord.ext.commands import Bot, Cog
+from discord.ext import commands
+from datetime import datetime
+import calendar
+import random
+import re
+
+class Dnd(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def stats(self, ctx):
+        """Roll 4d6 dropping lowest for D&D"""
+        # TODO: over/below average
+        message = f"{ctx.author.mention} your stats are: \n-----------\n"
+        full_stats = []
+        for x in range(6):
+            stat = []
+            for y in range(4):
+                num = random.randint(1, 6)
+                stat.append(num)
+            message += f"{stat[0]} + {stat[1]} + {stat[2]} + {stat[3]} - **{sum(stat) - min(stat)}**\n"
+            full_stats.append(sum(stat) - min(stat))
+        full_stats.sort()
+        message += f"\n{full_stats[0]}, {full_stats[1]}, {full_stats[2]}, {full_stats[3]}, {full_stats[4]}, {full_stats[5]}"
+        await ctx.send(message)
