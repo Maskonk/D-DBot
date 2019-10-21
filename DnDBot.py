f59a9c66c69f5512dd1b49e9b00dd6db1b4b1cd1
Maskonk <pincer1@hotmail.co.uk>
2019-10-22 00:17:13 +0100

    Added suggestions command

--- DnDBot.py
+++ DnDBot.py
@@ -13,6 +13,13 @@ async def on_ready():
     print("Online")
 
 
+@client.command(aliases=["Suggestions", "Suggest", "suggest"])
+async def suggestions(ctx):
+    """Message pertaining to suggestions for the bot."""
+    await ctx.send("If you have any suggestions for new features or an existing feature that could be made better feel "
+                   "free to message Punky with your idea. (No promises it will be implemented though)")
+
+
 @client.command(pass_context=True)
 async def git(ctx):
     """Alternate command for those trying to get the github link."""
